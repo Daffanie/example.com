fa6cac21897a881ba161172ce2f318f371269af7
Daffanie Hurley <daffhur50@gmail.com>
2019-01-07 15:47:21 -0600

    Added a form validation

--- public/contact.php
+++ public/contact.php
@@ -1,79 +1,123 @@
-<br><br><br><br>
 <?php
-$data = $_POST;
 
-foreach($data as $key => $value){
-  echo "{$key} = {$value}<br>";
+require '../core/Daffanie/src/Validation/validate.php';
+
+use Daffanie\Validation;
+
+$message = null;
+$valid = new Daffanie\Validation\validate();
+
+$args = [
+  'name'=>FILTER_SANITIZE_STRING,
+  'subject'=>FILTER_SANITIZE_STRING,
+  'message'=>FILTER_SANITIZE_STRING,
+  'email'=>FILTER_SANITIZE_EMAIL,
+];
+
+$input = filter_input_array(INPUT_POST, $args);
+
+if(!empty($input)){
+  $valid->validation = [
+    'email'=>[[
+      'rule'=>'email',
+      'message'=>'Please enter a valid email'
+    ],[
+      'rule'=>'notEmpty',
+      'message'=>'Please enter an email'
+    ]],
+    'name'=>[[
+      'rule'=>'notEmpty',
+      'message'=>'Please enter a your name'
+    ]],
+    'message'=>[[
+      'rule'=>'notEmpty',
+      'message'=>'Please add a message'
+    ]]
+  ];
+
+  $valid->check($input);
+
+  if(empty($valid->errors)){
+    $message = "<div class=\"alert alert-success\">Your form has been submitted!</div>";
+  }else{
+    $message = "<div class=\"alert alert-danger\">Your form has errors!</div>";
+  }
 }
 
 ?>
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
     <title>Daffanie Hurley</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./dist/css/main.css">
-    <script src="main.js"></script>
-</head>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-danger">
+        <a class="navbar-brand" href="#">Daffanie Hurley</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-<body>
-    <header>
-        <span class="logo">Daffanie Hurley Website</span>
-        <a id="toggleMenu">Menu</a>
-            <nav>
-                <ul>
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="resume.php">Resume</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                </ul>
-            </nav>
-    </header>
-
-<main>
-
-    <form action="contact.php" method="POST">
-        <h1>Contact Form</h1>
-        <div>
-            <label for="name">Name</label>
-            <input id="name" type="text" name="name">
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="resume.php">Resume</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+            </ul>
         </div>
+    </nav>
+    <main class="container">
+    <br><br><br>
+    <?php echo $message; ?>
 
-        <div>
-            <label for="email">Email</label>
-            <input id="email" type="text" name="replyto">
-        </div>
+    <form action="contact.php" method="POST" novalidate>
+      <h1>Contact Form</h1>
 
-        <div>
-            <label for="message">Message</label>
-            <textarea id="message" name="message"></textarea>
-        </div>
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input class="form-control" id="name" type="text" name="name" value="<?php echo $valid->userInput('name'); ?>">
+        <div class="text-danger"><?php echo $valid->error('name'); ?></div>
+      </div>
 
-        <div>
-            <input type="hidden" name="subject" value="New submission!">
-        </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input required class="form-control" id="email" type="email" name="email" value="<?php echo $valid->userInput('email'); ?>">
+        <div class="text-danger"><?php echo $valid->error('email'); ?></div>
+      </div>
 
-        <div>
-            <input type="submit" value="Send">
-        </div>
-     </form>
-     <script>
-            var toggleMenu = document.getElementById('toggleMenu');
-          var nav = document.querySelector('nav');
-          toggleMenu.addEventListener(
-            'click',
-            function(){
-              if(nav.style.display=='block'){
-                nav.style.display='none';
-              }else{
-                nav.style.display='block';
-              }
-            }
-          );
+      <div class="form-group">
+        <label for="message">Message</label>
+        <textarea class="form-control" id="message" name="message"><?php echo $valid->userInput('message'); ?></textarea>
+        <div class="text-danger"><?php echo $valid->error('message'); ?></div>
+      </div>
 
-    </script>
-</main>
-</body>
+      <div>
+        <input type="hidden" name="subject" value="New submission!">
+      </div>
+
+      <div>
+        <input type="submit" value="Send" class="btn btn-primary">
+      </div>
+
+    </form>
+    </main>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
 </html>
