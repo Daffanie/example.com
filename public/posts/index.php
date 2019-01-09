977646874927486abcadd66ab4b4b4d39660964c
Daffanie Hurley <daffhur50@gmail.com>
2019-01-09 16:24:41 -0600

    Added posts

---
+++ public/posts/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require '../../core/db_connect.php';
+
+
+$meta=[];
+$meta['title']="Bob's Blog";
+
+$content="<h1>Blog Posts</h1>";
+$stmt = $pdo->query('SELECT * FROM posts');
+
+while($row = $stmt ->fetch()){
+  $content .= "<a href=\posts/view.php?slug={$row['slug']}\">{$row['title']}</a>";
+}
+
+require '../../core/layout.php';
